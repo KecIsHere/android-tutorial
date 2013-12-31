0a80c17f815aa495d82e16761f4e6fd6a149a0dc
KecIsHere <kecmanpredrag@yahoo.com>
2013-12-31 14:56:20 +0100

    Finishing video 5

--- app/src/main/java/com/kec/tut/MainActivity.java
+++ app/src/main/java/com/kec/tut/MainActivity.java
@@ -10,19 +10,36 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends ActionBarActivity {
+
+    Button myButton;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
-        }
+        textView = (TextView)findViewById(R.id.textView);
+
+        myButton = (Button)findViewById(R.id.ourButton);
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String text = textView.getText().toString();
+                if(text.contains("Android"))
+                {
+                    textView.setText("Hello World");
+                }
+                else
+                {
+                    textView.setText("Hello Android");
+                }
+            }
+        });
     }
 
 
