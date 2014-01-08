e6c920911ddf62f552d8e70eeb2823615234b9a9
KecIsHere <kecmanpredrag@yahoo.com>
2014-01-08 20:40:38 +0100

    Finishing Video 9 - Added actions and icons on menu items

--- app/src/main/java/com/kec/tut/MainActivity.java
+++ app/src/main/java/com/kec/tut/MainActivity.java
@@ -66,7 +66,7 @@ public class MainActivity extends ActionBarActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        
+
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
@@ -77,11 +77,26 @@ public class MainActivity extends ActionBarActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+
+        switch(item.getItemId())
+        {
+            case R.id.twitter:
+                Log.i(TAG, "Twitter item clicked");
+                return true;
+            case R.id.facebook:
+                Log.i(TAG, "Facebook item clicked");
+                return true;
+            case R.id.refresh:
+                Log.i(TAG, "Refresh item clicked");
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
-        return super.onOptionsItemSelected(item);
+//        int id = item.getItemId();
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//        return super.onOptionsItemSelected(item);
     }
 
     /**
